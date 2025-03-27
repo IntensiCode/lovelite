a392f898f4e9e3898cc3e6b0b44c957fdfca818f
Daniel Lukic <daniel.lukic@gmx.net>
2025-03-27 06:43:33 +0100

    Add weapons.lua to have enemy weapons in one place

---
+++ src/enemy/weapons.lua
@@ -0,0 +1,52 @@
+local table_utils = require("src.base.table")
+
+---@class Weapon
+---@field name string
+---@field damage number
+---@field cooldown number
+---@field speed number
+---@field initial boolean
+---@field tile table
+local weapons = {
+    fist = {
+        name = "fist",
+        melee = 5,
+        cooldown = 0.3,
+        speed = 1.0,
+        initial = true
+    },
+    bite = {
+        name = "bite",
+        melee = 5,
+        cooldown = 0.6,
+        speed = 1.0,
+        initial = true
+    },
+    sonic = {
+        name = "sonic",
+        damage = 10,
+        cooldown = 1.0,
+        speed = 1.0,
+        initial = true
+    },
+    strongsonic = {
+        name = "strongsonic",
+        damage = 25,
+        cooldown = 2.5,
+        speed = 1.0,
+        initial = true
+    }
+}
+
+---Initialize weapons from map manager
+---@param map_manager table The map manager instance
+function weapons.init(map_manager)
+    -- Add all weapons from map manager
+    for gid, weapon_props in pairs(map_manager.weapons) do
+        local weapon = table_utils.clone(weapon_props)
+        weapon.tile = weapon_props.tile
+        weapons[weapon.name] = weapon
+    end
+end
+
+return weapons 
